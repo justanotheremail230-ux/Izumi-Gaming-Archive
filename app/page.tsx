9d640e16922d5056777e814cb7810c00d2448ffa
Shreyansh <justanotheremail230@gmail.com>
2026-08-16 19:19:34 +0530

    Remove Spotify widget

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import FadeInSection from "./components/FadeInSection";
 import GameCard from "./components/GameCard";
 import DecryptTitle from "./components/DecryptTitle";
 import SocialHUD from "./components/SocialHUD";
-import MemoriesSection from "./components/MemoriesSection";
-import SpotifyWidget from "./components/SpotifyWidget"; // NEW: Spotify widget import
+import MemoriesSection from "./components/MemoriesSection"; // NEW: Memories import
 
 // Batch fetch IGDB covers & summaries for all sections in 1 single call
 async function getBatchGameData(gameNames: string[]) {
@@ -108,7 +107,7 @@ export default async function Home() {
   // ==========================================
 
   return (
-    <main className="min-h-screen bg-gradient-to-b from-black via-zinc-950 to-black text-white relative">
+    <main className="min-h-screen bg-gradient-to-b from-black via-zinc-950 to-black text-white">
       <Navbar />
 
       {/* HERO */}
@@ -227,9 +226,6 @@ export default async function Home() {
           <SearchableGameGrid games={libraryWithData} />
         </section>
       </FadeInSection>
-
-      {/* FLOATING SPOTIFY WIDGET */}
-      <SpotifyWidget />
     </main>
   );
 }
